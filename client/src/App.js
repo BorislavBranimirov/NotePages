1e1105e0396aa0e6d4e243531ce34c7916cee785
Borislav Branimirov <boriintorr@gmail.com>
2020-04-22 02:26:12 +0300

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import Home from './components/Home';
 import LoginForm from './components/LoginForm';
 import SignUpForm from './components/SignUpForm';
@@ -8,9 +9,7 @@ const App = () => {
     return (
         <BrowserRouter>
             <div>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/login'}>Login</Link>
-                <Link to={'/signup'}>Sign up</Link>
+                <NavBar />
                 <Switch>
                     <Route exact path='/' component={Home} />
                     <Route path='/login' component={LoginForm} />
