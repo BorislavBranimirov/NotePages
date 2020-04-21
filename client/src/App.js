967ae3abc595cc92fec99d29ea7c8b693f580ce2
Borislav Branimirov <boriintorr@gmail.com>
2020-04-22 01:40:38 +0300

    added login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
     return (
         <BrowserRouter>
             <div>
                 <Link to={'/'}>Home</Link>
+                <Link to={'/login'}>Login</Link>
                 <Switch>
-                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/login' component={LoginForm} />
                 </Switch>
             </div>
         </BrowserRouter>
