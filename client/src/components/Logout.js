ce91df4aa60a6a381d7bd0b59160d8cf44442f47
Borislav Branimirov <boriintorr@gmail.com>
2020-04-24 13:46:27 +0300

    added redirect to logout button

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import { logout } from '../../utils/authUtils';
 
 const Logout = (props) => {
+    let history = useHistory();
+
     const handleClick = (event) => {
         logout();
+        return history.push('/');
     };
 
     return (
