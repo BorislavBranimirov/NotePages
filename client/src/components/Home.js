88c7624d74d9c8eebce0a8e86e5965012cf37e3d
Borislav Branimirov <boriintorr@gmail.com>
2020-05-11 23:18:44 +0300

    fixed Firefox input field flickering and home page punctuation

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
             <p>NotePages is a note-taking app where all of the notes are styled like dynamic notebook pages.</p>
             <p>To get started, go to the sign-up page and create a new account.</p>
             <p>
-                After you've logged in, navigate to the notes page, where all of your notes will be listed.
+                After you've logged in, navigate to the notes page where all of your notes will be listed.
                 There, you can search for notes and sort them by date of creation or title.
             </p>
             <p>
